8b81372a96c08bdd7bfd0c34cd24546f464fe18f
Nicolae Casandra <nicasandra@endava.com>
2016-11-11 17:19:14 +0200

    First problem

---
+++ src/first/PrimeNumbers.java
@@ -0,0 +1,60 @@
+package first;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by nicasandra on 11/11/2016.
+ */
+public class PrimeNumbers implements Runnable {
+    List<Integer> source;
+    List<Integer> destination;
+
+    public List<Integer> getSource() {
+        return source;
+    }
+
+    public void setSource(List<Integer> source) {
+        this.source = source;
+    }
+
+    public List<Integer> getDestination() {
+        return destination;
+    }
+
+    public void setDestination(List<Integer> destination) {
+        this.destination = destination;
+    }
+
+    public PrimeNumbers(List<Integer> source, List<Integer> destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public PrimeNumbers() {
+    }
+
+    @Override
+    public synchronized void run() {
+        for (Integer elem : source) {
+            if (isPrime(elem)) {
+                destination.add(elem);
+                System.out.println(Thread.currentThread().getName() + "   " + elem);
+            }
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
